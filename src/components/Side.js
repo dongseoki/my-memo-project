5bdf17a2e3f552d80ba81b493328fcc011ddfcbd
이동석 ezpmp 연구원 <dslee@ezpmp.co.kr>
2025-06-04 19:08:34 +0900

    .

--- src/components/Side.js
+++ src/components/Side.js
@@ -2,12 +2,16 @@ import React from "react";
 import SideHeader from "./SideHeader";
 import SideBody from "./SideBody";
 
-function Side({ memos }) {
+function Side({ memos, selectedMemoIdx, setSelectedMemoIdx }) {
   return (
     <div className="Side">
       Side
       <SideHeader />
-      <SideBody memos={memos} />
+      <SideBody
+        memos={memos}
+        selectedMemoIdx={selectedMemoIdx}
+        setSelectedMemoIdx={setSelectedMemoIdx}
+      />
     </div>
   );
 }
